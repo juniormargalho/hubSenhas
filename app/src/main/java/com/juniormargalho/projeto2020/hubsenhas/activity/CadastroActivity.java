11a45794b7ba66e4e07eee6d8ec84ca9f4f1992e
juniormargalho <juniormargalho@gmail.com>
2020-07-09 18:39:09 -0300

    tela de cadastro

--- app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/CadastroActivity.java
+++ app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/CadastroActivity.java
@@ -3,14 +3,29 @@ package com.juniormargalho.projeto2020.hubsenhas.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.juniormargalho.projeto2020.hubsenhas.R;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText editCadastroNome, editCadastroEmail, editCadastroEmailConfirmar, editCadastroSenha, editCadastroSenhaConfirmar;
+    private Button buttonCadastroCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        getSupportActionBar().hide();
+
+        inicializar();
+    }
+
+    private void inicializar(){
+        editCadastroNome = findViewById(R.id.editCadastroNome);
+        editCadastroEmail = findViewById(R.id.editCadastroEmail);
+        editCadastroEmailConfirmar = findViewById(R.id.editCadastroEmailConfirmar);
+        editCadastroSenha = findViewById(R.id.editCadastroSenha);
+        editCadastroSenhaConfirmar = findViewById(R.id.editCadastroSenhaConfirmar);
     }
 }
--- app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/LoginActivity.java
+++ app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/LoginActivity.java
@@ -2,16 +2,39 @@ package com.juniormargalho.projeto2020.hubsenhas.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.juniormargalho.projeto2020.hubsenhas.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editLoginEmail, editLoginSenha;
+    private Button buttonLoginEntrar;
+    private TextView textLoginCadastrese;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        inicializar();
+    }
+
+    public void cadastrese(View view){
+        Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void inicializar(){
+        editLoginEmail = findViewById(R.id.editLoginEmail);
+        editLoginSenha = findViewById(R.id.editLoginSenha);
+        buttonLoginEntrar = findViewById(R.id.buttonLoginEntrar);
+        textLoginCadastrese = findViewById(R.id.textLoginCadastrese);
     }
 }
