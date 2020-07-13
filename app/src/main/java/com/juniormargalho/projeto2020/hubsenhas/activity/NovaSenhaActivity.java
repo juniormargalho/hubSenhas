c2009c9328847874ebda0de2c45587935533d0a4
juniormargalho <juniormargalho@gmail.com>
2020-07-12 23:30:13 -0300

    tela de nova senha

--- app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/NovaSenhaActivity.java
+++ app/src/main/java/com/juniormargalho/projeto2020/hubsenhas/activity/NovaSenhaActivity.java
@@ -8,10 +8,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.juniormargalho.projeto2020.hubsenhas.R;
 
 public class NovaSenhaActivity extends AppCompatActivity {
+    private EditText editNovaSenhaTitulo, editNovaSenhaLogin, editNovaSenhaSenha, editNovaSenhaObs;
+    private RadioButton radioNovaSenha1, radioNovaSenha2, radioNovaSenha3, radioNovaSenha4, radioNovaSenha5;
+    private Button buttonNovaSenhaGerar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,8 @@ public class NovaSenhaActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        inicializar();
     }
 
     @Override
@@ -35,5 +44,18 @@ public class NovaSenhaActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_nova_senha, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void inicializar(){
+        editNovaSenhaTitulo = findViewById(R.id.editNovaSenhaTitulo);
+        editNovaSenhaLogin = findViewById(R.id.editNovaSenhaLogin);
+        editNovaSenhaSenha = findViewById(R.id.editNovaSenhaSenha);
+        editNovaSenhaObs = findViewById(R.id.editNovaSenhaObs);
+        radioNovaSenha1 = findViewById(R.id.radioNovaSenha1);
+        radioNovaSenha2 = findViewById(R.id.radioNovaSenha2);
+        radioNovaSenha3 = findViewById(R.id.radioNovaSenha3);
+        radioNovaSenha4 = findViewById(R.id.radioNovaSenha4);
+        radioNovaSenha5 = findViewById(R.id.radioNovaSenha5);
+        buttonNovaSenhaGerar = findViewById(R.id.buttonNovaSenhaGerar);
     }
 }
